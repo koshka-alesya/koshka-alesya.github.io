8aeb1e25d3b0e489243b66f8c8226649d90f840b
koshka-alesya <veronica.desmond@mail.ru>
2019-09-22 23:21:02 +0300

    new

---
+++ precache-manifest.ba4425bc82bb9f164c49883f7385a453.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fab9e95379da2fdf48c496379d718e4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a664befc13be3eaef92",
+    "url": "/static/css/main.bc231845.chunk.css"
+  },
+  {
+    "revision": "775d7812734894cc5bbe",
+    "url": "/static/js/2.1a94c416.chunk.js"
+  },
+  {
+    "revision": "9a664befc13be3eaef92",
+    "url": "/static/js/main.715d42a9.chunk.js"
+  },
+  {
+    "revision": "a1b897a764c034922dc4",
+    "url": "/static/js/runtime~main.2a6d3704.js"
+  },
+  {
+    "revision": "a5ce0e501fac6665614ce3d042e9e040",
+    "url": "/static/media/DSC07778.a5ce0e50.jpg"
+  },
+  {
+    "revision": "f22805d5469f91b55010dda1174ba031",
+    "url": "/static/media/DSC_0099.f22805d5.jpg"
+  },
+  {
+    "revision": "6df5df4f7a63508bb5be21ba4483033b",
+    "url": "/static/media/DSC_0206.6df5df4f.jpg"
+  },
+  {
+    "revision": "0cb320958c58b74ad8ccd89b38b6af4f",
+    "url": "/static/media/DSC_0320.0cb32095.jpg"
+  },
+  {
+    "revision": "8583799fbc65371512195ef97bcab932",
+    "url": "/static/media/DSC_0335.8583799f.jpg"
+  },
+  {
+    "revision": "81b2ce02f0455e884903f88783d9942e",
+    "url": "/static/media/DSC_0747.81b2ce02.jpg"
+  },
+  {
+    "revision": "5ee1b7f84485ad53dfba5d62dcaf848d",
+    "url": "/static/media/DSC_1802.5ee1b7f8.jpg"
+  },
+  {
+    "revision": "64b9ef97e3598a76b237bcaa6417e00a",
+    "url": "/static/media/DSC_2701.64b9ef97.jpg"
+  },
+  {
+    "revision": "9daa03226705c09678f3d0ac2a0d931a",
+    "url": "/static/media/DSC_2719.9daa0322.jpg"
+  },
+  {
+    "revision": "98a737ac7701ce263558ad9a7c9f8238",
+    "url": "/static/media/DSC_2840.98a737ac.jpg"
+  },
+  {
+    "revision": "2675f0ef61d1faba94334a290782a1f4",
+    "url": "/static/media/DSC_3515-2.2675f0ef.jpg"
+  },
+  {
+    "revision": "902047ed8861c4665f9f977f68eccde5",
+    "url": "/static/media/DSC_6659.902047ed.jpg"
+  },
+  {
+    "revision": "2fe27d9d10cdfccb1baef28a45d5ba90",
+    "url": "/static/media/Lato-Light.2fe27d9d.ttf"
+  },
+  {
+    "revision": "2d36b1a925432bae7f3c53a340868c6e",
+    "url": "/static/media/Lato-Regular.2d36b1a9.ttf"
+  },
+  {
+    "revision": "6416447dc3bceaf9d38ecbc29499fe08",
+    "url": "/static/media/about.6416447d.jpg"
+  },
+  {
+    "revision": "82867b3d3025c8fe7fd80a59891fb6f9",
+    "url": "/static/media/insta.82867b3d.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d157b2683dbf4817a889faf2d838f2ac",
+    "url": "/static/media/logo.d157b268.svg"
+  },
+  {
+    "revision": "84d58e3a9a7b3cc5e95141c840814ac3",
+    "url": "/static/media/messages.84d58e3a.svg"
+  },
+  {
+    "revision": "fac7e3dad2f9e401d3c724233b67a954",
+    "url": "/static/media/photo-126.fac7e3da.jpg"
+  },
+  {
+    "revision": "19500214eb74be9b1b674fa9fc423582",
+    "url": "/static/media/photo-134.19500214.jpg"
+  },
+  {
+    "revision": "9adda98216bcdd6f2cb975766cb7ad71",
+    "url": "/static/media/photo-199.9adda982.jpg"
+  },
+  {
+    "revision": "5d71062f16b4c0877a0b3dae8e1486a9",
+    "url": "/static/media/photo-230.5d71062f.jpg"
+  },
+  {
+    "revision": "974ee2b574d1cc59268d52d294f2ed7b",
+    "url": "/static/media/photo-239.974ee2b5.jpg"
+  },
+  {
+    "revision": "e1ed8a434a93b2e295f17b59e2a7ab8c",
+    "url": "/static/media/photo-248.e1ed8a43.jpg"
+  },
+  {
+    "revision": "d3b71b27ee6a4f3dbf3e2a5ad9dd42c6",
+    "url": "/static/media/slava.d3b71b27.jpg"
+  },
+  {
+    "revision": "b3d758c9a09fa25b2001c6aafc1b761e",
+    "url": "/static/media/vk.b3d758c9.svg"
+  }
+]);
